ddb01379a6a33869514ad62c6465a71f9283c687
Cynthia Nikolai <cynthianikolai@gmail.com>
2021-10-19 17:47:01 -0400

    Added diceware interfaces.

--- src/main/java/edu/cnm/deepdive/dicewareservice/controller/HelloWorldController.java
+++ src/main/java/edu/cnm/deepdive/dicewareservice/controller/HelloWorldController.java
@@ -2,7 +2,6 @@ package edu.cnm.deepdive.dicewareservice.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,13 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
 
   @GetMapping(value = "greet") //endpoint
-  public String greet(@RequestParam(name = "t", required = false, defaultValue = "World!") String target) { //http://localhost:8080/greet?t=World
+  public String greet(String target) {
     return String.format("Hello, %s", target); //http://localhost:8080/greet?target=World
-  }
-
-  @GetMapping("goodjob")
-  public String goodJob() {
-    return "You're building a webserver then soon onto a webservice, congratulations!";
   }
 
 }
