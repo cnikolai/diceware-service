563fe9a4bb2c3bf41114476e13103528a1a0d158
Cynthia Nikolai <cynthianikolai@gmail.com>
2021-10-20 13:32:34 -0400

    MVP working - generates passphrases.

--- src/main/java/edu/cnm/deepdive/dicewareservice/controller/PassphraseController.java
+++ src/main/java/edu/cnm/deepdive/dicewareservice/controller/PassphraseController.java
@@ -4,6 +4,7 @@ import edu.cnm.deepdive.dicewareservice.service.PassphraseGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,14 +13,14 @@ public class PassphraseController {
 
   private final PassphraseGenerator generator;
 
-  @Autowired //looking for PassphrseGenerator to inject into constructor
+  @Autowired //looking for PassphraseGenerator to inject into constructor
   public PassphraseController(
       PassphraseGenerator generator) {
     this.generator = generator;
   }
 
   @GetMapping("/transient") //transient => this is not meant for database storage
-  public String[] generate(int length) {
+  public String[] generate(@RequestParam(defaultValue = "5") int length) {
     return generator.generate(length);
   }
 }
